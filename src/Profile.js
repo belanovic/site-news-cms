1b807f2151c8c9c68f06f796c09b66eed9649528
Goran Belanovic <goranbelanovic@yahoo.com>
2021-07-19 03:32:22 +0200

    'msg'

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,6 @@
 import react, { useContext, useEffect } from 'react';
 import {context} from './newsContext';
+import './style/profile.css'
 
 export default function Profile({loggedUser}) {
 
@@ -21,19 +22,19 @@ export default function Profile({loggedUser}) {
         <div className = "profile">
             <div className = "profile-element">
                 <div className = "profile-element-description">Korisničko ime</div>
-                <div className = "profile-element-username">{localStorage.getItem('loggedUsername')}</div>
+                <div className = "profile-element-username data">{localStorage.getItem('loggedUsername')}</div>
             </div>
             <div className = "profile-element">
                 <div className = "profile-element-description">Ime</div>
-                <div className = "profile-element-firstname">{localStorage.getItem('loggedFirstName')}</div>
+                <div className = "profile-element-firstname data">{localStorage.getItem('loggedFirstName')}</div>
             </div>
             <div className = "profile-element">
                 <div className = "profile-element-description">Prezime</div>
-                <div className = "profile-element-lastname">{localStorage.getItem('loggedLastName')}</div>
+                <div className = "profile-element-lastname data">{localStorage.getItem('loggedLastName')}</div>
             </div>
             <div className = "profile-element">
                 <div className = "profile-element-description">email</div>
-                <div className = "profile-element-email">{localStorage.getItem('loggedEmail')}</div>
+                <div className = "profile-element-email data">{localStorage.getItem('loggedEmail')}</div>
             </div>
             <button 
                 className = "profile-signOut"
