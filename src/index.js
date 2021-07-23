f4f60bed53157e91a4aec7630f6a59b2044d6d71
goran <goranbelanovic@yahoo.com>
2021-07-23 23:45:15 +0200

    msg

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import App from './App.js';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from './newsContext';
 import './style/index.css';
-import './style/media.css';
-
 
 ReactDom.render(<Provider><Router><App /></Router></Provider>, document.getElementById('root'));
