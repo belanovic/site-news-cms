49e7a9208b935655501ec45e6bd578d8173c7104
Goran Belanovic <goranbelanovic@yahoo.com>
2021-08-14 01:48:50 +0200

    'msg'

--- src/Facebook.js
+++ src/Facebook.js
@@ -2,19 +2,22 @@ import react from 'react';
 
 export default function Facebook() {
 
-
-    FB.api(
+    const callFace = () => {
+           FB.api(
         '/105289518528554/feed',
         'POST',
         {"message":"hello, ovo je drugi post"},
         function(response) {
-            // Insert your code here
+            console.log('fejs je ovo')
         }
       );
+    }
+ 
 
 
     return (
         <div className = "facebook">
+            <button onClick = {() => callFace()}>Facebook call</button>
         </div>
     )
 }
