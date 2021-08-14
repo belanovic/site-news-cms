0b66887f555c43444069bd1d721fc9645ca86fcd
Goran Belanovic <goranbelanovic@yahoo.com>
2021-08-14 03:14:05 +0200

    'msg'

--- src/Facebook.js
+++ src/Facebook.js
@@ -3,7 +3,7 @@ import react from 'react';
 export default function Facebook() {
 
     function callFace () {
-        console.log('iz poziva' + ' ' + FB)
+        console.log(FB)
            FB.api(
         '/105289518528554/feed',
         'POST',
