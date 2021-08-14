fe0dc82e88428d1047c843d361f511a65e6365cb
Goran Belanovic <goranbelanovic@yahoo.com>
2021-08-14 04:53:20 +0200

    'msg'

--- src/Facebook.js
+++ src/Facebook.js
@@ -3,7 +3,7 @@ import react, {useEffect} from 'react';
 
 export default function Facebook() {
 
-    useEffect(function() {
+   /*  useEffect(function() {
         window.fbAsyncInit = function() {
           window.FB.init({
             appId            : '623982548570883',
@@ -12,23 +12,12 @@ export default function Facebook() {
             version          : 'v11.0'
           });
       
-          // Now that we've initialized the JavaScript SDK, we call
-          // FB.getLoginStatus().  This function gets the state of the
-          // person visiting this page and can return one of three states to
-          // the callback you provide.  They can be:
-          //
-          // 1. Logged into your app ('connected')
-          // 2. Logged into Facebook, but not your app ('not_authorized')
-          // 3. Not logged into Facebook and can't tell if they are logged into
-          //    your app or not.
-          //
-          // These three cases are handled in the callback function.
           window.FB.getLoginStatus(function(response) {
             this.statusChangeCallback(response);
           }.bind(this));
         }.bind(this);
       
-        // Load the SDK asynchronously
+
         (function(d, s, id) {
           var js, fjs = d.getElementsByTagName(s)[0];
           if (d.getElementById(id)) return;
@@ -37,18 +26,25 @@ export default function Facebook() {
           fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
       }, []) 
-    
+     */
     
 
-    function callFace () {
-        window.FB.api(
+     async function callFace () {
+ /*        window.FB.api(
         '/105289518528554/feed',
         'POST',
         {"message":"hello, ovo je post iz cms-a"},  
         function(response) {
             console.log(response)
         }
-      );
+      ); */
+let r = await fetch( "https://graph.facebook.com/v11.0/105289518528554/feed?message=1010101010&access_token=EAAI3gkInUwMBAK3KXdU7Oa96bhGOqhpnXQojoZAZAZCCCZBUnHzMUNKR1bL64jRfjdd943GPBiLdW5BZBgQDOZAnKzsSmsyoVOiFTxabCCcUY16GvjS8psdGEtne8iPFBVIvQ6GyrVl62AlC76BtP9XZBJiBBtQNmzQ9sE2QfBWZAFZB5KUsaw3I0Ea5zpvGrVPJuYZBqv20Ab9BtdRgFH60ce", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'text/plain'
+    }
+})
+console.log(r)
     }
     return (
         <div className = "facebook"> 
