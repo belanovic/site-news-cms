8e25ff6ba75098869cf5b5506009f1602831b220
Goran Belanovic <Goran Belanovic>
2021-08-17 20:00:52 +0200

    'msg'

--- src/Search.js
+++ src/Search.js
@@ -94,7 +94,10 @@ export default function Search({option}) {
                 onClick = {handleClick}
             >{requestSent? 'Pretraživanje...' : 'Traži'}
             </button>
-            <i className="fas fa-search"></i>
+            <i
+                onClick = {handleClick}
+                className = {`fas fa-search ${requestSent && 'sending'}`}
+            ></i>
         </div>
     )
 }
