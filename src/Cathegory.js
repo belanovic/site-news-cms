f5e0fd0da0f69a1e4baaf437a3ea7f5290c67797
Goran Belanovic <goranbelanovic@yahoo.com>
2021-08-30 15:32:25 +0200

    'msg'

--- src/Cathegory.js
+++ src/Cathegory.js
@@ -100,7 +100,7 @@ export default function Cathegory({ setPageNum, sortArticles }) {
                     <option value="business">Business</option>
                     <option value="technology">Technology</option>
                     <option value="entertainment">Entertainment</option>
-                    <option value="sports">Sports</option>
+                    <option value="sports">Sports</option>   
                 </select>
             </div>
             <div className="cathegory-button">
