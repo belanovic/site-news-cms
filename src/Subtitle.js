4382a4d649c66083a0cf14b87696d8dd93ce877b
Goran Belanovic <goranbelanovic@yahoo.com>
2021-09-06 13:29:37 +0200

    'msg'

--- src/Subtitle.js
+++ src/Subtitle.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Subtitle({ subtitle, setSubtitle }) {
 
-    function handleChangeSubtitle(e) {
+    function handleChangeSubtitle(e) { 
         const value = e.target.value; 
         setSubtitle(value);
     }
 
     return (
-        <div className="article-text-subtitle">
+        <div className="article-text-subtitle"> 
             <textarea
                 type='text'
                 name="subtitle"
