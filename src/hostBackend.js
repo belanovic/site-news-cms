64ee97f7df8c7207c80249ab4f5f73cbe6c4b8b0
Goran Belanovic <goranbelanovic@yahoo.com>
2021-11-08 04:21:47 +0100

    'msg'

--- src/hostBackend.js
+++ src/hostBackend.js
@@ -1,6 +1,8 @@
 // const HOST_BACKEND = 'https://site-news-backend.herokuapp.com';
-// const HOST_BACKEND = 'http://localhost:4000';
+/// const HOST_BACKEND = 'http://localhost:4000';
 
 const HOST_BACKEND = process.env.HOST_BACKEND || 'http://localhost:4000';
+
+console.log(HOST_BACKEND)
 
 export default HOST_BACKEND;
